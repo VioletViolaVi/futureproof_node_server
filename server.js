eb06f69fa212ceb8eaa5f8a7773cae84c60cd01b
vivian <vivian.v18723@gmail.com>
2022-07-14 12:14:25 +0100

    add listen to server

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+// console.log(http);
+
+// takes 2 params. 1 = a request, 2 = a response
+const server = http.createServer((req, rep) => {
+  // server logic
+});
+
+server.listen(80);
